3f6f88c9e02d8e4c50ce1231acab544f4525702c
wonderluc <workmaildo@yandex.ru>
2021-08-29 14:01:54 +0500

    feat: add page flip

--- react-magic/src/Pagination/Pagination.tsx
+++ react-magic/src/Pagination/Pagination.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import { api } from '../Api';
+import { useCardsContext } from '../state/state';
+import { Actions } from '../types';
 
 interface IPagainationProps {
   children: JSX.Element;
@@ -7,6 +9,24 @@ interface IPagainationProps {
 
 export default function Pagination(props: IPagainationProps): JSX.Element {
   const [page, setPage] = useState(1);
+  const context = useCardsContext();
+
+  function flipPage (e: React.MouseEvent<HTMLButtonElement>, n: number): void {
+    e.preventDefault();
+    const currentPage = page + n;
+    if (currentPage < 1 ) return;
+    api.setOptions({
+      page: currentPage,
+    });
+    api.setStatus(false);
+    requestCards();
+    setPage(currentPage);
+  }
+
+  async function requestCards(): Promise<void> {
+    await api.downolad();
+    context.dispacth({ type: Actions.CARDS_UPDATE, data: api.cards });
+  }
 
   return (
     <div className="pagination">
@@ -16,9 +36,9 @@ export default function Pagination(props: IPagainationProps): JSX.Element {
       </label>
       { props.children }
       <div className="page-counter">
-        <button className="page-counter__prev">-</button>
+        <button className="page-counter__prev" onClick={ (e) => flipPage(e, -1) }>Previous Page</button>
         <p>{ page }</p>
-        <button className="page-counter__next">+</button>
+        <button className="page-counter__next" onClick={ (e) => flipPage(e, 1) }>Next Page</button>
       </div>
     </div>
   );
