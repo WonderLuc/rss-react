d30843696f46a9843ef05c3ed11b426013ed31c6
wonderluc <workmaildo@yandex.ru>
2021-08-29 14:38:58 +0500

    feat: add page size

--- react-magic/src/Pagination/Pagination.tsx
+++ react-magic/src/Pagination/Pagination.tsx
@@ -28,11 +28,28 @@ export default function Pagination(props: IPagainationProps): JSX.Element {
     context.dispacth({ type: Actions.CARDS_UPDATE, data: api.cards });
   }
 
+  function changeCardsOnPage (e: React.ChangeEvent<HTMLInputElement>): void {
+    const value = parseInt(e.target.value);
+    if (!value || value < 12 || value > 100) return;
+    api.setOptions({
+      pageSize: value,
+    });
+    requestCards();
+  }
+
   return (
     <div className="pagination">
       <label htmlFor="pageSize">
         Cards on page
-        <input type="number" max="100" min="10" id="pageSize" value={api.options.pageSize} />
+        <input type="number" max="100" min="10" id="pageSize"
+          defaultValue={api.options.pageSize}
+          onChange={
+            (e) => {
+              e.preventDefault();
+              changeCardsOnPage(e);
+            }
+          }
+          />
       </label>
       { props.children }
       <div className="page-counter">
