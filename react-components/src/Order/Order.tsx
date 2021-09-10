171221c6beae0cb5c9bd6637753f84adc8045687
wonderLuc <workmaildo@yandex.ru>
2021-09-10 23:15:22 +0500

    feat: add strict eslint

--- react-components/src/Order/Order.tsx
+++ react-components/src/Order/Order.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { IOrder } from '../types';
 import './style.scss';
 
-export default function Order(props: IOrder): JSX.Element {
+export default function Order({
+  pack, id, name, date, office,
+}: IOrder): JSX.Element {
   return (
-    <li className={props.pack ? 'order order_packed' : 'order'}>
-      {props.pack && <img src="./assets/gift.png" alt="" className="order__pack" /> }
-      <p className="order__id">Order {props.id}</p>
-      <p className="order__name">{props.name}</p>
-      <p className="order__date">{props.date}</p>
-      <p className="order__office">{props.office}</p>
+    <li className={pack ? 'order order_packed' : 'order'}>
+      {pack && <img src="./assets/gift.png" alt="" className="order__pack" /> }
+      <p className="order__id">
+        Order
+        {id}
+      </p>
+      <p className="order__name">{name}</p>
+      <p className="order__date">{date}</p>
+      <p className="order__office">{office}</p>
     </li>
   );
 }
